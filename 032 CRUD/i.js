b6127dbd4ab4cc9c9e7b9a071dd350a7e616dc3d
Dovydas9314 <odovydas@gmail.com>
2024-04-22 15:51:29 +0300

    032 CRUD

---
+++ 032 CRUD/i.js
@@ -0,0 +1,16 @@
+console.log('Labas CRUD (create read update delete)')
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+window.addEventListener('load', _ => {
+
+
+
+
+
+});
