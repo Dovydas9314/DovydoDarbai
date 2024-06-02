85b04aff379c71a23e335a1e855f0061ec2a8376
Dovydas9314 <odovydas@gmail.com>
2024-06-03 00:01:49 +0300

    new

--- 049/index.js
+++ 049/index.js
@@ -8,12 +8,18 @@ const PORT = 80;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/add', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'add-animal.html'));
+});
+
+app.get('/edit', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'edit-animal.html'));
 });
 
 app.get('/api/animals', (req, res) => {
@@ -61,7 +67,63 @@ app.post('/api/animals', (req, res) => {
     });
 });
 
+app.put('/api/animals/:index', (req, res) => {
+    const index = parseInt(req.params.index, 10);
+    const updatedAnimal = req.body;
+    const filePath = path.join(__dirname, 'animals.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading JSON file', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        let animals = JSON.parse(data);
+        if (index >= 0 && index < animals.length) {
+            animals[index] = updatedAnimal;
+            fs.writeFile(filePath, JSON.stringify(animals, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error('Error writing JSON file', err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.json(updatedAnimal);
+                }
+            });
+        } else {
+            res.status(404).send('Animal not found');
+        }
+    });
+});
+
+app.delete('/api/animals/:index', (req, res) => {
+    const index = parseInt(req.params.index, 10);
+    const filePath = path.join(__dirname, 'animals.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading JSON file', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        let animals = JSON.parse(data);
+        if (index >= 0 && index < animals.length) {
+            const deletedAnimal = animals.splice(index, 1);
+            fs.writeFile(filePath, JSON.stringify(animals, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error('Error writing JSON file', err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.json(deletedAnimal[0]);
+                }
+            });
+        } else {
+            res.status(404).send('Animal not found');
+        }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
